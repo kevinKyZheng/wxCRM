0d22f9b2448d7acb1668756342c884c07605e627
zhengkaiyuan <zhengkaiyuan@chinawutong.com>
2018-09-29 11:27:49 +0800

    update

---
+++ pages/journal/journal.js
@@ -0,0 +1,21 @@
+Page({
+    data: {
+        array: [{
+                "title": "写日总结",
+                "icon": "rizongjie"
+            },
+            {
+                "title": "写日计划",
+                "icon": "rijihua"
+            },
+            {
+                "title": "写周总结",
+                "icon": "zhongzongjie"
+            },
+            {
+                "title": "写周计划",
+                "icon": "zhoujihua"
+            },
+        ]
+    }
+})
